27df44297d97284b95cbe00b49707a8281639cd2
Frédéric Delhoume <delhoume@gmail.com>
2021-04-09 18:20:45 +0200

    Add files via upload
    sources

---
+++ DigitalRainDemo2.ino
@@ -0,0 +1,21 @@
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include "DigitalRainAnim.h"
+
+TFT_eSPI tft = TFT_eSPI();
+DigitalRainAnim digitalRainAnim = DigitalRainAnim(); 
+ 
+void setup() {
+  Serial.begin(115200);
+  SPIFFS.begin();
+  listFiles(); // Lists the files so you can see what is in the SPIFFS
+
+  tft.begin();
+  tft.setRotation(2);
+  tft.fillScreen(TFT_BLACK);
+  digitalRainAnim.init(&tft);
+}
+
+void loop() {
+  digitalRainAnim.loop();
+}
